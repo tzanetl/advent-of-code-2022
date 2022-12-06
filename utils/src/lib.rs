e0132a6d0bd500e0272d95261181c807a6cc7537
Taneli <taneli.leppanen@hotmail.com>
2022-12-06 21:12:43 +0200

    Add logging setup to utils lib

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,6 +1,8 @@
 use std::fs;
 use std::path::Path;
 
+use log;
+use simple_logger;
 
 fn read_file(filepath: &Path) -> String {
     if filepath.exists() == false {
@@ -10,7 +12,6 @@ fn read_file(filepath: &Path) -> String {
     return content;
 }
 
-
 fn parse_input_file_path(args: &Vec<String>) -> &Path {
     if args.contains(&String::from("--test")) {
         return Path::new("test_input.txt")
@@ -19,12 +20,20 @@ fn parse_input_file_path(args: &Vec<String>) -> &Path {
     }
 }
 
-
 pub fn read_input(args: &Vec<String>) -> String {
     let filepath = parse_input_file_path(args);
     read_file(filepath)
 }
 
+pub fn set_logging_level(args: &Vec<String>) {
+    let level: log::Level;
+    if args.contains(&String::from("--test")) {
+        level = log::Level::Debug;
+    } else {
+        level = log::Level::Info;
+    }
+    simple_logger::init_with_level(level).expect("Failed to init logger")
+}
 
 #[cfg(test)]
 mod tests {
