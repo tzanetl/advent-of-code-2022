238210b43bd460a1eb5df70cafba51736c4191db
Taneli <taneli.leppanen@hotmail.com>
2023-03-11 17:43:41 +0200

    Day 15 part 2, partial

--- day_15/src/main.rs
+++ day_15/src/main.rs
@@ -1,5 +1,8 @@
-use std::env;
+use std::{env, time::Instant};
+use std::ops::RangeInclusive;
 use std::collections::HashSet;
+use std::thread;
+use std::sync::mpsc;
 
 use log::debug;
 use lazy_static::lazy_static;
@@ -7,7 +10,7 @@ use regex::Regex;
 
 use utils::{read_input, set_logging_level};
 
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 struct Sensor {
     x: i32,
     y: i32,
@@ -27,19 +30,15 @@ impl Sensor {
         let range = distance([&x_sensor, &y_sensor], [&x_beacon, &y_beacon]);
         return (Sensor { x: x_sensor, y: y_sensor, range }, Beacon{x: x_beacon, y: y_beacon})
     }
-
-    pub fn in_range(&self, x: &i32, y: &i32) -> bool {
-        return self.range >= distance([&self.x, &self.y], [x, y])
-    }
 }
 
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 struct Beacon {
     x: i32,
     y: i32
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Map {
     sensors: HashSet<Sensor>,
     beacons: HashSet<Beacon>,
@@ -73,25 +72,116 @@ impl Map {
         return Map { sensors, beacons, x_min, x_max }
     }
 
-    pub fn no_beacons(&self, y: i32) -> u32 {
+    pub fn width(&self) -> u32 {
+        return self.x_min.abs_diff(self.x_max)
+    }
+
+    fn covers(&self, sensor: &Sensor, y: &i32) -> Option<RangeInclusive<usize>> {
+        let width: i32 = (sensor.range as i32 * 2 + 1) - (y.abs_diff(sensor.y) * 2) as i32;
+        if width < 1 {
+            return None
+        }
+        let offset: i32 = ((width - 1) / 2) as i32;
+        let start: usize = match (sensor.x - offset + self.x_min.abs()).try_into() {
+            Ok(value) => value,
+            Err(_) => self.x_min.try_into().unwrap()
+        };
+        let mut end: usize = (sensor.x + offset + self.x_min.abs()).try_into().unwrap();
+        if end >= self.width() as usize {
+            end = (self.width() - 1) as usize;
+        }
+        return Some(RangeInclusive::new(start, end))
+    }
+
+    fn covered(&self, y: i32) -> Vec<bool> {
         debug!("y={}", y);
-        let mut count: u32 = 0;
-        for x in self.x_min..=self.x_max {
-            for sensor in &self.sensors {
-                // If sensor is in range, that tile cannot contain a beacon
-                if sensor.in_range(&x, &y) & !self.beacons.contains(&Beacon{ x, y }) {
-                    count += 1;
-                    debug!("count={}, x={}", count, x);
-                    break;
+        let mut row = vec![true; self.width() as usize];
+        debug!("{:?}", row.len());
+        for sensor in &self.sensors {
+            if let Some(sensor_range) = self.covers(sensor, &y) {
+                debug!("{:?}", sensor_range);
+                for i in sensor_range {
+                    row[i] = false;
                 }
             }
         }
-        return count;
+        return row;
     }
+
+    pub fn no_beacons(&self, y: i32) -> usize {
+        let mut row = self.covered(y);
+
+        for beacon in &self.beacons {
+            if beacon.y == y {
+                row[(beacon.x + self.x_min.abs()) as usize] = true;
+            };
+        };
+        debug!("{:?}", &row);
+        debug!("{:?}", row.len());
+        return row.iter().filter(|&n| *n == false).count()
+    }
+
+}
+
+fn beacon_frequency(map: Map) -> Option<i32>{
+    let threads: i32 = 1;
+    println!("Using {} threads", threads);
+    let mut bounds: Vec<i32> = Vec::new();
+    let step = map.x_max.abs_diff(map.x_min) as i32 / threads;
+
+    for i in 0..threads {
+        bounds.push(map.x_min + i * step);
+    }
+    bounds.push(map.x_max);
+    debug!("Bounds: {:?}", bounds);
+
+    let (tx, rx) = mpsc::channel();
+
+    for t in 0..(threads as usize) {
+        let map_clone = map.clone();
+        let tx_clone = tx.clone();
+        let start = bounds[t];
+        let end = bounds[t + 1];
+        thread::spawn(move || {
+            match find_in_range(map_clone, start, end) {
+                Some(point) => tx_clone.send(Some(point)).expect("Unable to return point"),
+                None => match tx_clone.send(None) {
+                    Ok(_) => println!("Thread returned ok"),
+                    Err(_) => println!("Thread didn't return ok")
+                }
+                
+            }
+        });
+    }
+
+    for received in rx {
+        match received {
+            Some(point) => {
+                println!("Point: {:?}", point);
+                return Some(point[0] * 4000000 + point[1])
+            },
+            None => continue
+        }
+    }
+    return None;
+}
+
+fn find_in_range(map: Map, start: i32, end: i32) -> Option<[i32; 2]> {
+    for y in start..=end {
+        let covered = map.covered(y);
+        if covered.contains(&true) {
+            let x: i32 = covered.iter().position(|&v| v == true).unwrap() as i32;
+            debug!("Range {}..={}", start, end);
+            debug!("Possible beacon x={} y={}", x, y);
+            return Some([x, y]);
+        }
+    }
+    debug!("No empty positions found in rows {} to {}", start, end);
+    return None;
 }
 
 fn distance(start: [&i32; 2], end: [&i32; 2]) -> u32 {
-    return ((start[0] - end[0]).abs() + (start[1] - end[1]).abs()).try_into().unwrap();
+    return (start[0].abs_diff(*end[0]) + start[1].abs_diff(*end[1])).try_into().unwrap();
 }
 
 fn main() {
@@ -100,16 +190,37 @@ fn main() {
     let input = read_input(&args);
 
     let y: i32;
+    let min_range: i32 = 0;
+    let max_range: i32;
     if args.contains(&"--test".to_string()) {
         y = 10;
+        max_range = 20;
     } else {
         y = 2000000;
+        max_range = 4000000;
     }
 
-    let map = Map::from_input(&input);
+    let mut map = Map::from_input(&input);
     debug!("{:?}", map);
+
+    // Part 1
+    let start = Instant::now();
     let count = map.no_beacons(y);
+    let duration = start.elapsed();
     println!("On line y = {}, {} position(s) cannot contain beacons", y, count);
+    println!("Part 1 took: {:?}", duration);
+
+    // Part 2
+    // 666405357
+    // "correct"
+    // found solution (2889605, 3398893)
+    // answer 11558423398893
+
+    // Todo remove these
+    map.x_min = min_range;
+    map.x_max = max_range;
+    let frequency = beacon_frequency(map).unwrap();
+    println!("Tuning frequency is {}", frequency);
 }
 
 #[cfg(test)]
@@ -118,8 +229,128 @@ mod tests {
 
     #[test]
     fn test_sensor_from_line() {
-        let (sensor, beacon) = Sensor::from_line(&"Sensor at x=2, y=18: closest beacon is at x=-2, y=15");
+        let (sensor, beacon) = Sensor::from_line(
+            &"Sensor at x=2, y=18: closest beacon is at x=-2, y=15"
+        );
         assert_eq!(sensor, Sensor{ x: 2, y: 18, range: 7});
         assert_eq!(beacon, Beacon{ x: -2, y: 15,});
+    }
+
+    #[test]
+    fn test_beacon_frequency() {
+        let args = vec!["--test".to_string()];
+        let input = read_input(&args);
+        set_logging_level(&args);
+        let mut map = Map::from_input(&input);
+        map.x_min = 10;
+        map.x_max = 15;
+        let frequency = beacon_frequency(map).unwrap();
+        println!("{}", frequency);
+    }
+
+    #[test]
+    fn test_sensor_no_beacons_y_9() {
+        let input = read_input(&vec!["--test".to_string()]);
+        let map = Map::from_input(&input);
+        let count = map.no_beacons(9);
+        assert!(count == 25);
+    }
+
+    #[test]
+    fn test_sensor_no_beacons_y_10() {
+        let input = read_input(&vec!["--test".to_string()]);
+        let map = Map::from_input(&input);
+        let count = map.no_beacons(10);
+        assert!(count == 26);
+    }
+
+    #[test]
+    fn test_sensor_no_beacons_y_11() {
+        let args = vec!["--test".to_string()];
+        // set_logging_level(&args);
+        let input = read_input(&args);
+        let map = Map::from_input(&input);
+        let count = map.no_beacons(11);
+        assert_eq!(count, 28);
+    }
+
+    #[test]
+    fn test_sensor_no_beacons_y_6() {
+        let args = vec!["--test".to_string()];
+        // set_logging_level(&args);
+        let input = read_input(&args);
+        let map = Map::from_input(&input);
+        let count = map.no_beacons(6);
+        assert_eq!(count, 25);
+    }
+
+    #[test]
+    fn test_sensor_no_beacons_example_1_row_7() {
+        // let args = vec!["--test".to_string()];
+        // set_logging_level(&args);
+        let map = Map {
+            beacons: HashSet::new(),
+            sensors: HashSet::from([Sensor{ x: 8, y: 7, range: 9}]),
+            x_min: -2,
+            x_max: 25
+        };
+        let count = map.no_beacons(7);
+        assert_eq!(count, 19)
+    }
+
+    #[test]
+    fn test_sensor_no_beacons_example_1_row_6() {
+        // let args = vec!["--test".to_string()];
+        // set_logging_level(&args);
+        let map = Map {
+            beacons: HashSet::new(),
+            sensors: HashSet::from([Sensor{ x: 8, y: 7, range: 9}]),
+            x_min: -2,
+            x_max: 25
+        };
+        let count = map.no_beacons(6);
+        assert_eq!(count, 17)
+    }
+
+    #[test]
+    fn test_sensor_no_beacons_example_1_row_19() {
+        // let args = vec!["--test".to_string()];
+        // set_logging_level(&args);
+        let map = Map {
+            beacons: HashSet::new(),
+            sensors: HashSet::from([Sensor{ x: 8, y: 7, range: 9}]),
+            x_min: -2,
+            x_max: 25
+        };
+        let count = map.no_beacons(19);
+        assert_eq!(count, 0)
+    }
+
+    #[test]
+    fn test_sensor_no_beacons_example_1_row_ne2() {
+        // let args = vec!["--test".to_string()];
+        // set_logging_level(&args);
+        let map = Map {
+            beacons: HashSet::new(),
+            sensors: HashSet::from([Sensor{ x: 8, y: 7, range: 9}]),
+            x_min: -2,
+            x_max: 25
+        };
+        let count = map.no_beacons(-2);
+        assert_eq!(count, 1)
+    }
+
+    #[test]
+    fn test_sensor_no_beacons_example_1_row_ne1() {
+        // let args = vec!["--test".to_string()];
+        // set_logging_level(&args);
+        let map = Map {
+            beacons: HashSet::new(),
+            sensors: HashSet::from([Sensor{ x: 8, y: 7, range: 9}]),
+            x_min: -2,
+            x_max: 25
+        };
+        let count = map.no_beacons(-1);
+        assert_eq!(count, 3)
     }
 }
